41284b0a2e2a45efd63f55ec1d88c6868dc96c5d
Paul Pietzko <paul.pietzko@icloud.com>
2025-02-01 15:14:29 +0100

    analytics tag wip 3

--- tags/nano-analytics/nano-analytics.ts
+++ tags/nano-analytics/nano-analytics.ts
@@ -1,55 +1,66 @@
 class NanoAnalytics extends HTMLElement {
-  private measurementId: string
+
+  private measurementId: string;
+  private userId: string | null;
+  private sessionId;
+  private projectId;
+
 
   constructor() {
-    super()
-    this.measurementId = this.getAttribute("measurement-id") || ""
+    super();
+    this.projectId = this.getAttribute("projectId");
+    this.userId = this.getAttribute("userId");
+    this.sessionId = localStorage.getItem('nanoAnalyticsSessionId') || crypto.randomUUID();
+    localStorage.setItem('nanoAnalyticsSessionId', this.sessionId);
   }
 
   connectedCallback() {
-    this.loadGoogleAnalytics()
-    this.trackPageView()
-    window.addEventListener("popstate", this.trackPageView.bind(this))
+    this.trackPageView();
+    window.addEventListener("popstate", this.trackPageView.bind(this));
+    // Listen for custom events
+    window.addEventListener("nanoAnalyticsEvent", this.trackEvent.bind(this));
   }
 
   disconnectedCallback() {
-    window.removeEventListener("popstate", this.trackPageView.bind(this))
+    window.removeEventListener("popstate", this.trackPageView.bind(this));
+    window.removeEventListener("nanoAnalyticsEvent", this.trackEvent.bind(this));
   }
 
-  private loadGoogleAnalytics() {
-    const script = document.createElement("script")
-    script.src = `https://www.googletagmanager.com/gtag/js?id=${this.measurementId}`
-    script.async = true
-    document.head.appendChild(script)
-
-    script.onload = () => {
-      window.dataLayer = window.dataLayer || []
-      function gtag(...args: any[]) {
-        window.dataLayer.push(args)
-      }
-      gtag("js", new Date())
-      gtag("config", this.measurementId)
-    }
+  trackPageView() {
+    fetch("/api/tags/analytics", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        projectId: this.projectId,
+        sessionId: this.sessionId,
+        userId: this.userId,
+        url: window.location.pathname,
+        referrer: document.referrer,
+        userAgent: navigator.userAgent,
+      }),
+    });
   }
 
-  private trackPageView() {
-    if (typeof window.gtag !== "undefined") {
-      window.gtag("event", "page_view", {
-        page_title: document.title,
-        page_location: window.location.href,
-        page_path: window.location.pathname,
-      })
-    }
+  trackEvent(e) {
+    fetch("/api/tags/analytics", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        projectId: this.projectId,
+        sessionId: this.sessionId,
+        userId: this.userId,
+        url: window.location.pathname,
+        referrer: document.referrer,
+        userAgent: navigator.userAgent,
+        eventName: e.detail.name,
+        eventData: e.detail.data,
+      }),
+    });
   }
 }
 
-customElements.define("nano-analytics", NanoAnalytics)
-
-declare global {
-  interface Window {
-    dataLayer: any[]
-    gtag: (...args: any[]) => void
-  }
-}
-
-export default NanoAnalytics
+customElements.define("nano-analytics", NanoAnalytics);
