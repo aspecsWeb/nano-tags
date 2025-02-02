220648d3e8c89d61a75e90ca1b25089c205b402d
Paul Pietzko <paul.pietzko@icloud.com>
2025-02-02 11:29:35 +0100

    move google tracking out of analytics tag

--- tags/nano-analytics/nano-analytics.ts
+++ tags/nano-analytics/nano-analytics.ts
@@ -1,12 +1,10 @@
 class NanoAnalytics extends HTMLElement {
-  private measurementId: string;
+  private projectId: string | null;
   private userId: string | null;
   private sessionId: string;
-  private projectId: string | null;
 
   constructor() {
     super();
-    this.measurementId = process.env.GOOGLE_ANALYTICS_MEASUREMENT_ID || "";
     this.projectId = this.getAttribute("projectId");
     this.userId = this.getAttribute("userId");
     this.sessionId = localStorage.getItem('nanoAnalyticsSessionId') || crypto.randomUUID();
@@ -14,7 +12,6 @@ class NanoAnalytics extends HTMLElement {
   }
 
   connectedCallback() {
-    this.loadGoogleAnalytics();
     this.trackPageView();
     window.addEventListener("popstate", this.trackPageView.bind(this));
     window.addEventListener("nanoAnalyticsEvent", this.trackEvent.bind(this));
@@ -25,51 +22,20 @@ class NanoAnalytics extends HTMLElement {
     window.removeEventListener("nanoAnalyticsEvent", this.trackEvent.bind(this));
   }
 
-  private loadGoogleAnalytics() {
-    const script = document.createElement("script");
-    script.src = `https://www.googletagmanager.com/gtag/js?id=${this.measurementId}`;
-    script.async = true;
-    document.head.appendChild(script);
-
-    script.onload = () => {
-      window.dataLayer = window.dataLayer || [];
-      function gtag(...args: any[]) {
-        window.dataLayer.push(args);
-      }
-      gtag("js", new Date());
-      gtag("config", this.measurementId);
-    };
-  }
-
   private trackPageView() {
-    if (typeof window.gtag !== "undefined") {
-      window.gtag("event", "page_view", {
-        page_title: document.title,
-        page_location: window.location.href,
-        page_path: window.location.pathname,
-      });
-
-      // Send to your API
-      this.sendToApi({
-        eventType: 'page_view',
-        page_title: document.title,
-        page_location: window.location.href,
-        page_path: window.location.pathname,
-      });
-    }
+    this.sendToApi({
+      eventType: 'page_view',
+      page_title: document.title,
+      page_location: window.location.href,
+      page_path: window.location.pathname,
+    });
   }
 
   private trackEvent(e: CustomEvent) {
-    if (typeof window.gtag !== "undefined") {
-      // Capture custom event and send to Google Analytics
-      window.gtag("event", e.detail.name, e.detail.data);
-
-      // Send to your API
-      this.sendToApi({
-        eventType: e.detail.name,
-        event_data: e.detail.data,
-      });
-    }
+    this.sendToApi({
+      eventType: e.detail.name,
+      event_data: e.detail.data,
+    });
   }
 
   private sendToApi(data: any) {
@@ -92,11 +58,4 @@ class NanoAnalytics extends HTMLElement {
 
 customElements.define("nano-analytics", NanoAnalytics);
 
-declare global {
-  interface Window {
-    dataLayer: any[];
-    gtag: (...args: any[]) => void;
-  }
-}
-
-export default NanoAnalytics;
+export { NanoAnalytics };
